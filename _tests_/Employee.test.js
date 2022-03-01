2fa5b989f3ff7c08246418baf464b3df169c8f55
Tiffany Bryan <tiffanym.bryan@gmail.com>
2022-03-01 01:42:41 -0500

    update

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -1,7 +1,19 @@
-const Employee = require('./Employee')
+const Employee = require('../lib/Employee')
 
 test('create employee name', () => {
     const employee = new Employee('Kadeem');
 
     expect(employee.name).toBe('Kadeem');
+});
+
+test('create employee id', () => {
+    const employee = new Employee('Kadeem');
+
+    expect(employee.getId()).toEqual(expect.any(Number));
+});
+
+test('create a employee email', () => {
+    const employee = new Employee('Kadeem');
+
+    expect(employee.getEmail()).toBe('KadeemMCclendon@gmail.com');
 })
