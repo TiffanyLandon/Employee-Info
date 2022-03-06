659870fc4cf7ff2fec525aede37341423de02898
Tiffany Bryan <tiffanym.bryan@gmail.com>
2022-03-06 18:20:17 -0500

    update

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -1,19 +1,29 @@
 const Employee = require('../lib/Employee')
 
-test('create employee name', () => {
-    const employee = new Employee('Kadeem');
+test('create a employee ', () => {
+    const employee = new Employee('Kadeem', '1', 'tiffanym.bryan@gmail.com');
 
     expect(employee.name).toBe('Kadeem');
+    expect(employee.Id).toBe('1');
+    expect(employee.email).toBe('tiffanym.bryan@gmail.com');
 });
 
-test('create employee id', () => {
-    const employee = new Employee('Kadeem');
+test("get name of employee", () => {
+    const employee = new Employee('Kadeem')
 
-    expect(employee.getId()).toEqual(expect.any(Number));
+    expect(employee.getName()).toEqual(expect.stringContaining(employee.name))
+});
+
+
+test('create employee id', () => {
+    const employee = new Employee('Kadeem', '1', 'tiffanym.bryan@gmail.com');
+
+    expect(employee.getId()).toEqual(expect.stringContaining(employee.Id));
 });
 
 test('create a employee email', () => {
-    const employee = new Employee('Kadeem');
+    const employee = new Employee('Kadeem', '1', 'tiffanym.bryan@gmail.com');
 
-    expect(employee.getEmail()).toBe('KadeemMCclendon@gmail.com');
-})
+
+    expect(employee.getEmail()).toStrictEqual(expect.stringContaining(employee.email))
+});
