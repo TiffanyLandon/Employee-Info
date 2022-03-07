d784aacd1883827ca9fe13e25cd6ba8148842984
Tiffany Bryan <tiffanym.bryan@gmail.com>
2022-03-06 23:43:02 -0500

    Engineer

--- _tests_/Engineer.test.js
+++ _tests_/Engineer.test.js
@@ -1,13 +1,23 @@
 const Engineer = require('../lib/Engineer')
 
-test('create engineer name', () => {
-    const engineer = new Engineer('Cheryl','4','andy33056@yahoo.com','Cheryl');
+test('create a new Engineer', () => {
+    const engineer = new Engineer('Cheryl', '4', 'Landon124@gmail.com', "Cheryl234")
 
     expect(engineer.name).toBe('Cheryl');
-});
+    expect(engineer.Id).toBe('4');
+    expect(engineer.email).toBe('Landon124@gmail.com');
+    expect(engineer.github).toBe('Cheryl234')
 
-test('create a , () => {
-    const engineer = new Engineer('');
+})
 
-    expect(employee.getId()).toEqual(expect.any(Number));
+test('GitHub username', () => {
+    const engineer = new Engineer('Cheryl', '4', 'Landon124@gmail.com', "Cheryl234")
+
+    expect(engineer.getGithub()).toEqual(expect.stringContaining('Cheryl234'))
+})
+
+test('Engineer role', () => {
+    const engineer = new Engineer('Cheryl', '4', 'Landon124@gmail.com', "Cheryl234")
+
+    expect(engineer.getRole()).toEqual(expect.stringContaining('Engineer'))
 })
